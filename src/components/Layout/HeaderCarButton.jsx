7a3bab42f8b9c4dea9602ab886c42360698c2824
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-08-27 21:05:27 -0500

    Finished frontend part application

--- src/components/Layout/HeaderCarButton.jsx
+++ src/components/Layout/HeaderCarButton.jsx
@@ -1,20 +1,42 @@
-import React,{useContext} from 'react'
+import React,{useContext,useEffect} from 'react'
 import CartIcon from '../Cart/CarIcon'
 import classes from './HeaderCarButton.module.css';
 import CartContext from '../../store/cart-context'
+import { useState } from 'react';
+
 
 const HeaderCarButton = (props) => {
+
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false)
 
   const cartCtx = useContext(CartContext)
   const numberOfCartItems = cartCtx.items.reduce((curNumber,item) => {
     return curNumber + item.amount
   },0);
 
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ''}`
 
+  useEffect(() => {
+
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    },300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+
+  },[items]);
 
 
   return (
-    <button className={classes['button']} onClick = {props.onClick}>
+    <button className={btnClasses} onClick = {props.onClick}>
 
 
       <span className={classes.icon} >
